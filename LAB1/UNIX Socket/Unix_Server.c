d163805cb70bf50c6df39840646f4b2d44be5e1c
SARTHAK-JAIN-ASIC <sarthakj@usc.edu>
2025-01-18 19:30:56 -0800

    Update Unix_Server.c

--- LAB1/UNIX Socket/Unix_Server.c
+++ LAB1/UNIX Socket/Unix_Server.c
@@ -1,35 +1,40 @@
-/* a client in the unix domain */
+/* a server in the unix domain.  The pathname of 
+   the socket address is passed as an argument */
 #include <sys/types.h>
+#include <sys/socket.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <sys/socket.h>
 #include <sys/un.h>
 #include <stdio.h>
 void error(const char *);
-
 int main(int argc, char *argv[])
 {
-   int sockfd, servlen,n;
-   struct sockaddr_un  serv_addr;
-   char buffer[82];
+   int sockfd, newsockfd, servlen, n;
+   socklen_t clilen;
+   struct sockaddr_un  cli_addr, serv_addr;
+   char buf[80];
 
-   bzero((char *)&serv_addr,sizeof(serv_addr));
+   if ((sockfd = socket(AF_UNIX,SOCK_STREAM,0)) < 0)
+       error("creating socket");
+   bzero((char *) &serv_addr, sizeof(serv_addr));
    serv_addr.sun_family = AF_UNIX;
    strcpy(serv_addr.sun_path, argv[1]);
-   servlen = strlen(serv_addr.sun_path) + 
-                 sizeof(serv_addr.sun_family);
-   if ((sockfd = socket(AF_UNIX, SOCK_STREAM,0)) < 0)
-       error("Creating socket");
-   if (connect(sockfd, (struct sockaddr *) 
-                         &serv_addr, servlen) < 0)
-       error("Connecting");
-   printf("Please enter your message: ");
-   bzero(buffer,82);
-   fgets(buffer,80,stdin);
-   write(sockfd,buffer,strlen(buffer));
-   n=read(sockfd,buffer,80);
-   printf("The return message was\n");
-   write(1,buffer,n);
+   servlen=strlen(serv_addr.sun_path) + 
+                     sizeof(serv_addr.sun_family);
+   if(bind(sockfd,(struct sockaddr *)&serv_addr,servlen)<0)
+       error("binding socket"); 
+
+   listen(sockfd,5);
+   clilen = sizeof(cli_addr);
+   newsockfd = accept(
+        sockfd,(struct sockaddr *)&cli_addr,&clilen);
+   if (newsockfd < 0) 
+        error("accepting");
+   n=read(newsockfd,buf,80);
+   printf("A connection has been established\n");
+   write(1,buf,n);
+   write(newsockfd,"I got your message\n",19);
+   close(newsockfd);
    close(sockfd);
    return 0;
 }
